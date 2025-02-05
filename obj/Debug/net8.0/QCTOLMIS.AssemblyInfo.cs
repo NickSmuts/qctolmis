864e0845b11b167ec4ec736fded59cbdf6ea74a9
Nick Smuts <nick@efundisha.com>
2025-02-05 11:30:11 +0200

    Commit Restart Project

--- obj/Debug/net8.0/QCTOLMIS.AssemblyInfo.cs
+++ obj/Debug/net8.0/QCTOLMIS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QCTOLMIS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d06c76e0ac111366afc0222dd101f425f44ab966")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2471b61445f833f2d7e9b2441b3a52861c70189d")]
 [assembly: System.Reflection.AssemblyProductAttribute("QCTOLMIS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QCTOLMIS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
